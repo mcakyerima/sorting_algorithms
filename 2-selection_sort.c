776683c2f1aa44cf54c2dced1098c5f2bf7ae9ce
mcakyerima <mcakyerima@gmail.com>
2023-09-26 03:06:55 +0100

    selection_sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,53 @@
+#include "sort.h"
+
+/**
+ * File: 2-selection_sort.c
+ * Author: Mohammed Ak
+ */
+
+/**
+ * swap - swaps two numbers in place
+ * @a: the first number
+ * @b: the second number
+ **/
+
+void swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * selection_sort - sort array using selection the selection sort algorithm
+ * @array: The array to be sorted
+ * @size: Size of array to be sorted
+ *
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	unsigned int j, i , minimum;
+
+	if (array == NULL || size < 2)
+		return
+
+	for (i = 0; i < size; i++)
+	{
+		minimum = i;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[minimum] > array[j])
+				minimum = j;
+		}
+
+		if (minimum != i)
+		{
+			swap(&array[i], &array[minimum]);
+			print_array(array, size);
+		}
+	}
+}
--- sort.h
+++ sort.h
@@ -23,6 +23,7 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+void selection_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
