effccee088c3bb757fec91b743184f978db370c1
mcakyerima <mcakyerima@gmail.com>
2023-09-26 01:54:58 +0100

    initial commit

---
+++ 0-bubble_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+/*
+ * File: 0-bubble_sort.c
+ * Aurhor: Mohammed Ak
+ */
+
+/**
+ * bubble_sort - Sorts an array wity the bubble sort algorithm
+ * @size: The size of the array to sort
+ * @array: The array to be sorted
+ * Return: Null
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	int tmp;
+	size_t j, i;
+
+	if (array == NULL || size == 0)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
---
+++ sort.h
@@ -0,0 +1,34 @@
+#ifndef SORT_H
+#define SORT_H
+
+
+#include <stdlib.h>
+#include <stdlio.h>
+
+
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+
+/**
+ * struct listint_s - A Doubly linked list node
+ *
+ * @n: The Integer stored in the node
+ * @prev: A pointer to the previous element of the list
+ * @next: A pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+
+
+
+#endif/* SORT_H */
